1979cd618e271c45329c386966f92f191bb2f8f7
Lucas Simpson <lucassimpson05@gmail.com>
2016-07-06 23:20:24 -0400

    added user_token to form so it actually works now

--- ingest/forms.py
+++ ingest/forms.py
@@ -4,4 +4,4 @@ from .models import Event
 class EventForm(ModelForm):
     class Meta:
         model = Event
-        fields = ['domain', 'event_type', 'json_data']
+        fields = ['domain', 'event_type', 'json_data', 'user_token']
